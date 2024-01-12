86c8b1db628c8e9668207a642facb12c8e190a7b
brightonmboya <b.mboya@alustudent.com>
2024-01-12 12:20:27 +0300

    added the OG image generation

--- src/components/ui/oldHead.tsx
+++ src/components/ui/oldHead.tsx
@@ -1,8 +1,11 @@
-import Head from "next/head";
-
-export default function HeadSEO() {
+export default function Head() {
   return (
-    <Head>
+    <head>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-Y1V1WEYTND"
+      ></script>
+
       <meta http-equiv="x-ua-compatible" content="ie=edge" />
       <meta
         name="viewport"
@@ -20,6 +23,10 @@ export default function HeadSEO() {
       />
       <meta property="og:image:width" content="1080" />
       <meta property="og:image:height" content="1080" />
+      {/* <meta itemprop="name" content="Tazama Africa Tours and Safaris" />
+  <meta itemprop="url" content="https://www.tazamaafricasafari.com" />
+  <meta itemprop="description"
+    content="Helping you design the safari of your dreams. Tazama Africa designs unique travel experiences throughout East Africa, giving you the trip of a lifetime." /> */}
       <meta name="twitter:title" content="Tazama Africa Tours and Safaris" />
       <meta name="twitter:url" content="https://www.tazamaafricasafari.com" />
       <meta name="twitter:card" content="summary" />
@@ -30,13 +37,13 @@ export default function HeadSEO() {
       <meta
         name="description"
         content="Helping you design the safari of your dreams. Tazama Africa designs unique
-            travel experiences throughout East Africa, giving you the trip of a
-            lifetime."
+travel experiences throughout East Africa, giving you the trip of a
+lifetime."
       />
       <meta name="googlebot" content="notranslate" />
       <meta name="google" content="nopagereadaloud" />
       <meta name="robots" content="index, follow" />
       <title>Tazama Africa Tours and Safaris</title>
-    </Head>
+    </head>
   );
 }
