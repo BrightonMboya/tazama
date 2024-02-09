35b4b7cd60aaff220185f10e067c8b3c989d27bc
brightonmboya <b.mboya@alustudent.com>
2024-02-09 13:54:06 +0200

    adde the tailorMade blog

--- src/components/PrimaryHeader.tsx
+++ src/components/PrimaryHeader.tsx
@@ -2,28 +2,25 @@ type primaryHeader = {
   title: string;
   image: string;
   subTitle?: string;
-
 };
 
 const PrimaryHeader = (props: primaryHeader) => {
   return (
     <div className="relative h-screen">
       <div className="absolute bottom-0 left-0 right-0 top-0 flex flex-col items-start justify-center bg-black/60 text-white">
-        
         <img
           src={`/assets/images/gallery/${props.image}`}
           className="absolute left-0 right-0 top-0 -z-50 h-screen w-screen object-cover opacity-80"
         />
         <div className="container">
-          <p className="font-base absolute left-1/2 top-1/2 mx-auto w-full -translate-x-1/2 -translate-y-1/2 px-2 text-center text-6xl sm:px-12 lg:w-2/3 xl:text-7xl">
+          <h1 className="font-base absolute left-1/2 top-1/2 mx-auto w-full -translate-x-1/2 -translate-y-1/2 px-2 text-center text-6xl sm:px-12 lg:w-2/3 xl:text-7xl">
             {props.title}
-          </p>
+          </h1>
 
-         
           {props.subTitle && (
-            <p className="absolute bottom-10 left-1/2 -translate-x-1/2 text-center text-sm text-white">
+            <h3 className="absolute bottom-10 left-1/2 -translate-x-1/2 text-center text-sm text-white">
               {props.subTitle}
-            </p>
+            </h3>
           )}
         </div>
       </div>
