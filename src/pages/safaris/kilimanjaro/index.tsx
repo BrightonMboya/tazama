75c0b23d13ec8fc36e34c3a8a21ff19235d1af4c
brightonmboya <b.mboya@alustudent.com>
2024-03-05 12:41:20 +0300

    finished blog layout

--- src/pages/safaris/kilimanjaro/index.tsx
+++ src/pages/safaris/kilimanjaro/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import PrimaryHeader from "~/components/PrimaryHeader";
 import { setPageTitle } from "~/helpers";
 import useFetchImages from "~/hooks/useFetchImages";
-import  { ImageProps } from "~/lib/generateBlurPlaceHolder";
+import { ImageProps } from "~/lib/generateBlurPlaceHolder";
 import Gallery, { CloudinaryImage } from "~/components/ui/GalleryImage";
 
 const Page = ({ images }: { images: ImageProps[] }) => {
@@ -230,35 +230,7 @@ const Page = ({ images }: { images: ImageProps[] }) => {
 
 export default Page;
 export async function getStaticProps() {
-  // const results = await cloudinary.v2.search
-  //   .expression(`folder:kilimanjaro`)
-  //   .sort_by("public_id", "desc")
-  //   // .max_results(400)
-  //   .execute();
-  // let reducedResults: ImageProps[] = [];
-
-  // let i = 0;
-  // for (let result of results.resources) {
-  //   reducedResults.push({
-  //     id: i,
-  //     height: result.height,
-  //     width: result.width,
-  //     public_id: result.public_id,
-  //     format: result.format,
-  //   });
-  //   i++;
-  // }
-
-  // const blurImagePromises = results.resources.map((image: ImageProps) => {
-  //   return getBase64ImageUrl(image);
-  // });
-  // const imagesWithBlurDataUrls = await Promise.all(blurImagePromises);
-
-  // for (let i = 0; i < reducedResults.length; i++) {
-  //   reducedResults[i]!.blurDataUrl = imagesWithBlurDataUrls[i];
-  // }
   const images = await useFetchImages({ folderName: "kilimanjaro" });
-
   return {
     props: {
       images,
--- src/pages/safaris/kilimanjaro/routes/index.tsx
+++ src/pages/safaris/kilimanjaro/routes/index.tsx
@@ -1,14 +1,7 @@
-import React, { useEffect } from "react";
-import { setPageTitle } from "../../helpers";
 import Image from "next/legacy/image";
-import Link from "next/link";
 import { Button } from "~/components/ui/button";
 
 const ItenaryPage = () => {
-  useEffect(() => {
-    setPageTitle("Blogs");
-  }, []);
-
   return (
     <>
       <div className="relative h-[60vh]">
@@ -19,7 +12,7 @@ const ItenaryPage = () => {
           />
           <div className="container">
             <h1 className="font-base absolute left-1/2 top-1/2 mx-auto w-full -translate-x-1/2 -translate-y-1/2 px-2 text-center text-6xl sm:px-12 lg:w-2/3 xl:text-7xl">
-              Blog Title
+              A Journey to Africa's Highest Peak.
             </h1>
           </div>
         </div>
@@ -29,22 +22,24 @@ const ItenaryPage = () => {
         <section className="mx-auto flex items-start justify-center lg:mx-0 lg:gap-10">
           <div className="mx-auto  px-4 md:px-8 lg:mx-0 lg:max-w-[60%] lg:text-justify">
             <div className="mb-10 py-8">
-              <h3 className="text-4xl text-[#A87133]">Cultural Immersion</h3>
+              <h3 className="text-4xl text-[#A87133]">
+                Reach the Summit of Kilimanjaro with Tazama: A Journey to
+                Africa's Highest Peak
+              </h3>
               <p className="mb-3 mt-5 text-xl text-[#757371]">
-                <span className="text-primary">
-                  There’s something about the end of the day:{" "}
-                </span>
-                as the sun starts to dip, when a magical atmosphere descends
-                over the bush. The light softens to a golden glow, scents and
-                sounds seem to amplify and the landscape comes to life.
+                Nestled in Northern Tanzania, Mount Kilimanjaro stands as
+                Africa's tallest peak and the world's highest free-standing
+                mountain, soaring to an impressive 5,895 meters. Every year,
+                Kilimanjaro attracts around 30,000 trekkers, from seasoned
+                mountaineers to ambitious beginners, all drawn to the challenge
+                of reaching its summit.
               </p>{" "}
               <p className="mb-3 mt-5 text-xl text-[#757371]">
-                <span className="text-primary">
-                  There’s something about the end of the day:{" "}
-                </span>
-                as the sun starts to dip, when a magical atmosphere descends
-                over the bush. The light softens to a golden glow, scents and
-                sounds seem to amplify and the landscape comes to life.
+                Climbing Kilimanjaro with Tazama is an unforgettable adventure
+                that combines personal achievement with the raw beauty of
+                nature. The mountain offers a unique experience with its diverse
+                ecosystems, from lush rainforests to alpine deserts, each
+                providing its own set of challenges and rewards.
               </p>
               <div className="relative mt-[50px] h-[400px] w-full lg:hidden lg:w-[50%]">
                 <Image
@@ -55,20 +50,22 @@ const ItenaryPage = () => {
                 />
               </div>
               <p className="mb-3 mt-5 text-xl text-[#757371]">
-                <span className="text-primary">
-                  There’s something about the end of the day:{" "}
-                </span>
-                as the sun starts to dip, when a magical atmosphere descends
-                over the bush. The light softens to a golden glow, scents and
-                sounds seem to amplify and the landscape comes to life.
+                The journey to the top of Kilimanjaro is accessible to a wide
+                range of climbers, including young adventurers over the age of
+                10 and those in their 60s and 70s with a zest for life and
+                adventure. The main challenge lies in adapting to the altitude
+                and the pace of ascent. However, with Tazama's support, reaching
+                the summit is an attainable goal. Our expert guides are trained
+                to prioritize your safety and success, making them your most
+                valuable companions on this journey.
               </p>{" "}
               <p className="mb-3 mt-5 text-xl text-[#757371]">
                 <span className="text-primary">
-                  There’s something about the end of the day:{" "}
+                  So, who can climb Kilimanjaro? {"  "}
                 </span>
-                as the sun starts to dip, when a magical atmosphere descends
-                over the bush. The light softens to a golden glow, scents and
-                sounds seem to amplify and the landscape comes to life.
+                Anyone with a spirit of adventure and determination. Join the
+                exclusive group of climbers who have successfully summited Mount
+                Kilimanjaro, guided by the expertise of Tazama's dedicated team.
               </p>
               <div className="relative mt-[50px] h-[400px] w-full lg:hidden lg:w-[50%]">
                 <Image
@@ -79,20 +76,22 @@ const ItenaryPage = () => {
                 />
               </div>
               <p className="mb-3 mt-5 text-xl text-[#757371] lg:mt-0">
-                <span className="text-primary">
-                  There’s something about the end of the day:{" "}
-                </span>
-                as the sun starts to dip, when a magical atmosphere descends
-                over the bush. The light softens to a golden glow, scents and
-                sounds seem to amplify and the landscape comes to life.
+                <span className="text-primary">Best Time to Climb: </span>
+                Mount Kilimanjaro welcomes climbers year-round, offering the
+                chance to summit in any season. For those who prefer drier
+                conditions, the recommended months are January to February and
+                June to October. Selecting the best time to climb Kilimanjaro
+                requires careful consideration of weather patterns and other
+                factors, all of which Tazama is equipped to guide you through,
+                ensuring a remarkable experience.
               </p>{" "}
               <p className="mb-3 mt-5 text-xl text-[#757371]">
-                <span className="text-primary">
-                  There’s something about the end of the day:{" "}
-                </span>
-                as the sun starts to dip, when a magical atmosphere descends
-                over the bush. The light softens to a golden glow, scents and
-                sounds seem to amplify and the landscape comes to life.
+                Ready to witness the world from the peak of Africa and fulfill a
+                lifetime goal? Tazama is here to guide your journey to the
+                summit of Kilimanjaro. Our commitment to safety, personalized
+                service, and environmental stewardship ensures an unparalleled
+                climbing experience. Answer the call of the mountain and reach
+                the summit of Kilimanjaro with Tazama.
               </p>
             </div>
           </div>
